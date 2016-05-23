216071fd794a7ab06d0881d6f74305a9b3e18515
Саша <@ALEXANDR_PC>
2016-05-23 16:44:14 +0300

    Add project files.

---
+++ VK.Player/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace VK.Player
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.backgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.songsList = new System.Windows.Forms.ListBox();
+            this.CurrSong = new System.Windows.Forms.Label();
+            this.mediaPlayer = new AxWMPLib.AxWindowsMediaPlayer();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.mediaPlayer)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // backgroundWorker
+            // 
+            this.backgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // songsList
+            // 
+            this.songsList.FormattingEnabled = true;
+            this.songsList.ItemHeight = 16;
+            this.songsList.Location = new System.Drawing.Point(12, 12);
+            this.songsList.Name = "songsList";
+            this.songsList.Size = new System.Drawing.Size(461, 324);
+            this.songsList.TabIndex = 0;
+            this.songsList.MouseClick += new System.Windows.Forms.MouseEventHandler(this.songsList_MouseClick);
+            // 
+            // CurrSong
+            // 
+            this.CurrSong.AutoSize = true;
+            this.CurrSong.Enabled = false;
+            this.CurrSong.Location = new System.Drawing.Point(3, 0);
+            this.CurrSong.Name = "CurrSong";
+            this.CurrSong.Size = new System.Drawing.Size(68, 17);
+            this.CurrSong.TabIndex = 2;
+            this.CurrSong.Text = "No songs";
+            // 
+            // mediaPlayer
+            // 
+            this.mediaPlayer.Enabled = true;
+            this.mediaPlayer.Location = new System.Drawing.Point(3, 27);
+            this.mediaPlayer.Name = "mediaPlayer";
+            this.mediaPlayer.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("mediaPlayer.OcxState")));
+            this.mediaPlayer.Size = new System.Drawing.Size(219, 45);
+            this.mediaPlayer.TabIndex = 3;
+            this.mediaPlayer.PlayStateChange += new AxWMPLib._WMPOCXEvents_PlayStateChangeEventHandler(this.mediaPlayer_PlayStateChange);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.mediaPlayer);
+            this.panel1.Controls.Add(this.CurrSong);
+            this.panel1.Location = new System.Drawing.Point(12, 342);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(296, 104);
+            this.panel1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(485, 453);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.songsList);
+            this.Name = "Form1";
+            this.Text = "Vk.Player";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.mediaPlayer)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker;
+        private System.Windows.Forms.ListBox songsList;
+        private System.Windows.Forms.Label CurrSong;
+        private AxWMPLib.AxWindowsMediaPlayer mediaPlayer;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ VK.Player/Form1.cs
@@ -0,0 +1,102 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Web;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+namespace VK.Player
+{
+    public partial class Form1 : Form
+    {
+        private List<Audio> audioList;
+        WMPLib.IWMPPlaylist playList;
+        WMPLib.IWMPMedia media;
+        int index;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            new Form2().Show();
+            backgroundWorker.RunWorkerAsync();
+        }
+
+        public class Audio
+        {
+            public int aid { get; set; }
+            public int owner_id { get; set; }
+            public string artist { get; set; }
+            public string title { get; set; }
+            public int duration { get; set; }
+            public string url { get; set; }
+            public string lurlcs_id { get; set; }
+            public int genre { get; set; }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while(!Settings1.Default.auth) { Thread.Sleep(500); }
+
+            WebRequest request = WebRequest.Create("https://api.vk.com/method/audio.get?owner_id=" + Settings1.Default.id + "&need_user=0&access_token=" + Settings1.Default.token);
+            WebResponse response = request.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string responseFromServer = reader.ReadToEnd();
+            reader.Close();
+            response.Close();
+            responseFromServer = HttpUtility.HtmlDecode(responseFromServer);
+
+            JToken token = JToken.Parse(responseFromServer);
+            audioList = Enumerable.Skip(token["response"].Children(), 1).Select(c => c.ToObject<Audio>()).ToList();
+
+            this.Invoke((MethodInvoker)delegate
+            {
+                playList = mediaPlayer.playlistCollection.newPlaylist("vkPlayList");
+
+                for (int i = 0; i < audioList.Count(); i++)
+                {
+                    media = mediaPlayer.newMedia(audioList[i].url);
+                    playList.appendItem(media);
+
+                    songsList.Items.Add(audioList[i].artist + " - " + audioList[i].title);
+                }
+                mediaPlayer.currentPlaylist = playList;
+                mediaPlayer.Ctlcontrols.stop();
+            });
+        }
+
+        private void songsList_MouseClick(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Left)
+            {
+                mediaPlayer.Ctlcontrols.play();
+                mediaPlayer.Ctlcontrols.currentItem = mediaPlayer.currentPlaylist.get_Item(songsList.SelectedIndex);
+                index = songsList.SelectedIndex;
+                CurrSong.Text = songsList.SelectedItem.ToString();
+            }
+        }
+
+        private void mediaPlayer_PlayStateChange(object sender, AxWMPLib._WMPOCXEvents_PlayStateChangeEvent e)
+        {
+            switch (e.newState)
+            {
+                case 3:    // Playing
+                    CurrSong.Text = songsList.Items[index].ToString();
+                    break;
+                    
+                case 8:    // MediaEnded
+                    CurrSong.Text = " ";
+                    index++;
+                    break;
+            }
+        }
+    }
+}
